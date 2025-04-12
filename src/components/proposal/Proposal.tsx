17961b6d91cd0e195b295b10438e42151c5e89ac
rnldbss <ronaldbessam@gmail.com>
2025-04-12 09:57:51 +0100

    fix: add section button reloading page

--- src/components/proposal/Proposal.tsx
+++ src/components/proposal/Proposal.tsx
@@ -1,9 +1,30 @@
+"use client";
+
 import Form from "./Form";
+import Row from "./Row";
+import AddButton from "./buttons/AddButton";
+import { useState } from "react";
 
 export default function Proposal() {
+  const [addSection, setAddSection] = useState(1);
+
+  const handleAddSection = () => {
+    setAddSection((prev) => prev + 1);
+  };
+
+  const handleRemoveSection = () => {
+    setAddSection((prev) => prev - 1);
+  };
+
   return (
-    <>
-      <Form />
-    </>
+    <div className="flex flex-col">
+      <Form onClickDelete={handleRemoveSection} sectionsCount={addSection} />
+      <Row>
+        <AddButton
+          onClick={handleAddSection}
+          className="col-start-3 col-end-4"
+        />
+      </Row>
+    </div>
   );
 }
